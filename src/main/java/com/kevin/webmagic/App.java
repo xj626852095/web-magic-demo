c608f3856f60455911cd2c0c58cdef62177d38bf
xiangjie <xiangjie55@qq.com>
2016-07-13 23:58:01 +0800

    更新

---
+++ src/main/java/com/kevin/webmagic/App.java
@@ -0,0 +1,13 @@
+package com.kevin.webmagic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
